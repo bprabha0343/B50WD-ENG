449498ead5876f24d1bec455736ec43acf1b545e
bprabha0343 <116078743+bprabha0343@users.noreply.github.com>
2023-07-12 20:09:30 +0530

    adding third file

---
+++ third.js
@@ -0,0 +1,12 @@
+// // transpose of a matrix
+
+// // var arr = [[1,2,3],[4,5,6],[7,8,9]]
+// // var newArr =[]
+// // for (var i=0; i<arr.length; i++){
+// //     var temp = [];
+// //     for(var j=0; j<arr[i].length; j++){
+// //         temp.push(arr[j][i]);
+// //     }
+// //     newArr.push(temp);
+// // }
+// // console.log(newArr)
