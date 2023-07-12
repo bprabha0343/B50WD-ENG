2f125b501ee94fdab7dcf5eadb297b79c4099d98
bprabha0343 <116078743+bprabha0343@users.noreply.github.com>
2023-07-12 20:08:34 +0530

    adding first file

---
+++ first.js
@@ -0,0 +1,60 @@
+// // var n = 5;
+// // var sum = 0;
+// // for (var i=1; i<=n; i++){
+// //     sum+=i;
+// // }
+// // console.log(sum)
+
+// // 2d array to 1d array
+
+// // var arr = [[1,2,3],[4,5,6],[7,8,9]]
+// // var newArr=[]
+// // for (var i=0; i<arr.length; i++){
+// //     for (var j=0; j<arr[i].length;j++) {
+// //         newArr.push(arr[i][j])
+// //     }
+// // }
+// // console.log(newArr)
+
+// // transpose of a matrix
+
+// // var arr = [[1,2,3],[4,5,6],[7,8,9]]
+// // var newArr =[]
+// // for (var i=0; i<arr.length; i++){
+// //     var temp = [];
+// //     for(var j=0; j<arr[i].length; j++){
+// //         temp.push(arr[j][i]);
+// //     }
+// //     newArr.push(temp);
+// // }
+// // console.log(newArr)
+
+// // function solve(a,b){
+// //     return a+b
+// // }
+// // console.log(solve(2,3))
+
+// // a function that prints hello world 20 times
+
+// // function print(){
+// //     return "hello world"
+// // }
+// // var a  = print()
+// // for (i=0; i<=20; i++){
+// //     console.log(a)
+// // }
+// // find squares of element present in an array
+// var arr = [8,9,7,6,5,3]
+// var newarr = [];
+// for(var i=0; i<arr.length; i++){
+//     newarr.push(arr[i])
+//     var temp = []
+//     for (var j=0; j<arr; j++){
+//         temp.push(arr[j])
+//     }
+//     newarr.push(temp)
+// }
+// console.log(newarr)
+
+arr =[1,2,5,8,6,9,10]
+
